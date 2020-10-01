cb1556c1e1bbdfbc790194611cb1a438d11643ba
thomas viaules <thomas.viaules@gmail.com>
2020-10-01 17:39:52 -0400

    starting app from scratch

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,6 +9,10 @@ const SecondPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+    <br/><br/><br/><br/>
+    <div style={{ position: `sticky`, top: `0` }}> <p>THIS WILL STICK</p> </div>
+=    <p>Now go build something great.</p>
+    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
