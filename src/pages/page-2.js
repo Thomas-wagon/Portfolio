58762edc763a1a628c1363d7d12739438ad7669d
thomas viaules <thomas.viaules@gmail.com>
2020-10-02 14:06:01 -0400

    new gatsby portfolio setup and working, start building components

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,20 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <br/><br/><br/><br/>
-    <div style={{ position: `sticky`, top: `0` }}> <p>THIS WILL STICK</p> </div>
-=    <p>Now go build something great.</p>
-    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+const SecondPage = () => <p>page 2</p>
 
 export default SecondPage
