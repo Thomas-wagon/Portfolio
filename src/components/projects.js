95df8149135fe750cec02237376d9fa72b8cc2a3
thomas viaules <thomas.viaules@gmail.com>
2020-10-06 19:27:08 -0400

    work in progress

--- src/components/projects.js
+++ src/components/projects.js
@@ -9,46 +9,42 @@ const Projects = () => {
   return (
     <div className="projects">
       <h2>Projects</h2>
-      <div className="portfolio">
-        <div className="card">
-          <div className="photo">
-            <img src={Portfolio} alt="My portfolio" />
-          </div>
-          <div className="text">
-            <h3>
-              My Portfolio - <span className="date">Septembre 2020</span>
-            </h3>
-            <p>
-              I have designed my own portfolio on Figma, and built it as a
-              single page application with the static-site generator Gatsby.
-            </p>
-            <p>
-              I plan to improve its aesthetics as I progress with CSS,
-              javascript, React and pluggins libraries, by regularly adding new
-              effects and animations here and there.
-            </p>
-          </div>
+      <div className="card-left">
+        <div className="photo">
+          <img src={Portfolio} alt="My portfolio" />
+        </div>
+        <div className="text">
+          <h3>
+            My Portfolio - <span className="date">Septembre 2020</span>
+          </h3>
+          <p>
+            I have designed my own portfolio on Figma, and built it as a single
+            page application with the static-site generator Gatsby.
+          </p>
+          <p>
+            I plan to improve its aesthetics as I progress with CSS, javascript,
+            React and pluggins libraries, by regularly adding new effects and
+            animations here and there.
+          </p>
         </div>
       </div>
-      <div className="yummy-palette">
-        <div className="card">
-          <div className="text">
-            <h3>
-              My Portfolio - <span className="date">Septembre 2020</span>
-            </h3>
-            <p>
-              I have designed my own portfolio on Figma, and built it as a
-              single page application with the static-site generator Gatsby.
-            </p>
-            <p>
-              I plan to improve its aesthetics as I progress with CSS,
-              javascript, React and pluggins libraries, by regularly adding new
-              effects and animations here and there.
-            </p>
-          </div>
-          <div className="photo">
-            <img src={Yummy} alt="Yummy Palette" />
-          </div>
+      <div className="card-right">
+        <div className="text">
+          <h3>
+            My Portfolio - <span className="date">Septembre 2020</span>
+          </h3>
+          <p>
+            I have designed my own portfolio on Figma, and built it as a single
+            page application with the static-site generator Gatsby.
+          </p>
+          <p>
+            I plan to improve its aesthetics as I progress with CSS, javascript,
+            React and pluggins libraries, by regularly adding new effects and
+            animations here and there.
+          </p>
+        </div>
+        <div className="photo">
+          <img src={Yummy} alt="Yummy Palette" />
         </div>
       </div>
     </div>
