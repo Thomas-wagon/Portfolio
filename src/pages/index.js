8a60383ed4431b1f2d89734511c583027f8f7f62
thomas viaules <thomas.viaules@gmail.com>
2020-10-16 16:03:46 -0400

    test on meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,10 +26,7 @@ const IndexPage = () => {
           property="og:description"
           content="thomas viaules - web developer portfolio - thomas is a web developer based in montreal"
         />
-        <meta
-          property="og:image"
-          content="https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-        />
+        <meta property="og:image" content={Oglake} />
       </Helmet>
       <Banner2 />
       <Navbar />
