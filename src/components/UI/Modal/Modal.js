f4be7eeff6f2835a075fd2e7d59f15c977501c0a
thomas viaules <thomas.viaules@gmail.com>
2020-11-27 15:28:51 -0500

    improving closing modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -7,9 +7,12 @@ import ClosingButton from "../../../images/closing_button.png"
 const Modal = props => {
   return (
     <div className={`modal ${props.showModal ? "show" : ""}`}>
-      <div onClick={() => props.setShowModal(!props.showModal)}>
-        <img className="closing-button" src={ClosingButton} alt="close" />
-      </div>
+      <img
+        className="closing-button"
+        src={ClosingButton}
+        alt="close"
+        onClick={() => props.setShowModal(!props.showModal)}
+      />
       <h3>{props.title}</h3>
       {props.children}
     </div>
